34c9f1e03976e185fdf8c464c75399e9812cbbcf
teddy <nevercaution@gmail.com>
2020-01-21 21:58:16 +0900

    Body for BadRequest

--- demo-inflearn-api/src/main/java/com/nevercaution/demoinflearnapi/events/EventValidator.java
+++ demo-inflearn-api/src/main/java/com/nevercaution/demoinflearnapi/events/EventValidator.java
@@ -3,7 +3,6 @@ package com.nevercaution.demoinflearnapi.events;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Component
@@ -11,15 +10,14 @@ public class EventValidator {
 
     public void validate(EventDto eventDto, Errors errors) {
         if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0) {
-            errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong.");
-            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is wrong.");
+            errors.reject("wrongPrices", "Values of prices are wrong");  // global error
         }
 
         LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
         if (endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
         endEventDateTime.isBefore(eventDto.getCloseEnrollmentDatetime()) ||
         endEventDateTime.isBefore(eventDto.getBeginEnrollmentDatetime())) {
-            errors.rejectValue("endEventDateTime", "wrongValue", "endEventDateTime is wrong.");
+            errors.rejectValue("endEventDateTime", "wrongValue", "endEventDateTime is wrong.");  // field error
         }
 
         // TODO: beginEventDateTime
