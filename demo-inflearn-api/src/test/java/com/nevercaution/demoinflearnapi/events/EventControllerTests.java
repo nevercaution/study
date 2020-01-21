63d414181245cde52363ffa1bfa2d20bbc5d778e
teddy <nevercaution@gmail.com>
2020-01-21 16:23:48 +0900

    fail on unknown properties

--- demo-inflearn-api/src/test/java/com/nevercaution/demoinflearnapi/events/EventControllerTests.java
+++ demo-inflearn-api/src/test/java/com/nevercaution/demoinflearnapi/events/EventControllerTests.java
@@ -33,6 +33,35 @@ public class EventControllerTests {
 
     @Test
     public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("rest api development with spring")
+                .beginEnrollmentDatetime(LocalDateTime.of(2020, 1, 21, 14, 20))
+                .endEnrollmentDatetime(LocalDateTime.of(2020, 1, 22, 15, 0))
+                .beginEventDateTime(LocalDateTime.of(2020, 1, 21, 10, 0))
+                .endEventDateTime(LocalDateTime.of(2020, 1, 22, 15, 0))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrolment(100)
+                .location("강남역 D2 스타트업 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+    }
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -55,12 +84,6 @@ public class EventControllerTests {
                 .accept(MediaTypes.HAL_JSON)
                 .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+                .andExpect(status().isBadRequest());
     }
 }
