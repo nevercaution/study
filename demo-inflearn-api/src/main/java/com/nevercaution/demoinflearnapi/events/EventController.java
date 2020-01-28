52add15d0d0f307c213b0a7d9d76f01f74ec25bc
teddy <nevercaution@gmail.com>
2020-01-28 17:52:24 +0900

    add IndexController

--- demo-inflearn-api/src/main/java/com/nevercaution/demoinflearnapi/events/EventController.java
+++ demo-inflearn-api/src/main/java/com/nevercaution/demoinflearnapi/events/EventController.java
@@ -1,5 +1,6 @@
 package com.nevercaution.demoinflearnapi.events;
 
+import com.nevercaution.demoinflearnapi.common.ErrorsResource;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
@@ -40,12 +41,12 @@ public class EventController {
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
         if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         eventValidator.validate(eventDto, errors);
         if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         Event event = modelMapper.map(eventDto, Event.class);
@@ -59,5 +60,9 @@ public class EventController {
         eventResource.add(selfLinkBuilder.withRel("update-event"));
         eventResource.add(new Link("/docs/index.html#resources-events-create").withRel("profile"));
         return ResponseEntity.created(createdUri).body(eventResource);
+    }
+
+    private ResponseEntity badRequest(Errors errors) {
+        return ResponseEntity.badRequest().body(new ErrorsResource(errors));
     }
 }
