148a3f337b5c1f2270352c6aa291142a3050f78d
teddy <nevercaution@gmail.com>
2020-01-29 14:46:33 +0900

    add event page api

--- demo-inflearn-api/src/main/java/com/nevercaution/demoinflearnapi/events/EventController.java
+++ demo-inflearn-api/src/main/java/com/nevercaution/demoinflearnapi/events/EventController.java
@@ -3,12 +3,18 @@ package com.nevercaution.demoinflearnapi.events;
 import com.nevercaution.demoinflearnapi.common.ErrorsResource;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -60,6 +66,14 @@ public class EventController {
         eventResource.add(selfLinkBuilder.withRel("update-event"));
         eventResource.add(new Link("/docs/index.html#resources-events-create").withRel("profile"));
         return ResponseEntity.created(createdUri).body(eventResource);
+    }
+
+    @GetMapping
+    public ResponseEntity queryEvents(Pageable pageable, PagedResourcesAssembler<Event> assembler) {
+        Page<Event> page = eventRepository.findAll(pageable);
+        PagedModel<EntityModel<Event>> pagedModel = assembler.toModel(page, e -> new EventResource(e));
+        pagedModel.add(new Link("/docs/index.html#resources-events-list").withRel("profile"));
+        return ResponseEntity.ok().body(pagedModel);
     }
 
     private ResponseEntity badRequest(Errors errors) {
--- demo-inflearn-api/src/main/java/com/nevercaution/demoinflearnapi/events/EventResource.java
+++ demo-inflearn-api/src/main/java/com/nevercaution/demoinflearnapi/events/EventResource.java
@@ -1,22 +1,29 @@
 package com.nevercaution.demoinflearnapi.events;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 // beanSerializer 객체를 사용해서 serialize 한다.
-public class EventResource extends RepresentationModel {
+public class EventResource extends EntityModel<Event> {
 
-    @JsonUnwrapped // serialize 할 때 event 로 감싸지 않고 풀어주고 싶을 때 사용.
-    private Event event;
-
-    public EventResource(Event event) {
-        this.event = event;
+    public EventResource(Event event, Link... links) {
+        super(event, links);
         add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
     }
 
-    public Event getEvent() {
-        return event;
-    }
+//    @JsonUnwrapped // serialize 할 때 event 로 감싸지 않고 풀어주고 싶을 때 사용.
+//    private Event event;
+//
+//    public EventResource(Event event) {
+//        this.event = event;
+//        add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
+//    }
+//
+//    public Event getEvent() {
+//        return event;
+//    }
 }
